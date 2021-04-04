4c1cede03e8bab67cb3d003e700de7ed8d426ee7
117503445 <t117503445@gmail.com>
2021-04-04 16:49:20 +0800

    feat: add log
    1.0.5

--- router/router.go
+++ router/router.go
@@ -1,12 +1,11 @@
 package router
 
 import (
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
 	"share_board_backend/app/api"
 	"share_board_backend/app/middleware"
 	"share_board_backend/library/websocket"
-
-	"github.com/gogf/gf/frame/g"
-	"github.com/gogf/gf/net/ghttp"
 )
 
 func init() {
@@ -15,19 +14,21 @@ func init() {
 		group.Middleware(middleware.CORS, middleware.HttpLog)
 		group.ALL("/", api.Hello)
 		group.Group("/api", func(group *ghttp.RouterGroup) {
+
+			group.GET("/ws", func(r *ghttp.Request) {
+				g.Log().Line().Debug("LOG /api/ws")
+				if err := websocket.M.HandleRequest(r.Response.Writer, r.Request); err != nil {
+					g.Log().Line().Debug(err)
+				}
+			})
+			websocket.M.HandleMessage(api.PageOnMessage)
+
 			group.Group("/user", func(group *ghttp.RouterGroup) {
 				group.POST("/login", middleware.Auth.LoginHandler)
 				group.POST("/", api.User.SignUp)
 				group.Middleware(middleware.JWTLogin)
 				group.GET("/", api.User.GetInfo)
 			})
-
-			group.GET("/ws", func(r *ghttp.Request) {
-				if err := websocket.M.HandleRequest(r.Response.Writer, r.Request); err != nil {
-					g.Log().Line().Panic(err)
-				}
-			})
-			websocket.M.HandleMessage(api.PageOnMessage)
 
 			// crud_gen will insert here
 		})
